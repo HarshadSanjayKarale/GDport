39f8888b658e87145abacf3349437f4db687e446
HarshadSanjayKarale <harshadkarale0@gmail.com>
2024-03-10 20:21:58 +0530

    test4

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -26,14 +26,12 @@ function Navbar() {
             </label>
                 <img src={title}></img>
             <ul>
-
                 <li><Link to="/"><FaTachometerAlt />  Home</Link></li>
                 <li><Link to="/about"><FaAddressBook /> Publication</Link></li>
                 <li><a href='https://drive.google.com/drive/folders/1Tw2jrl7RBJQeBfp9DfU8kj7BxrT1ITSr?usp=drive_link'><FaChalkboard/> Study Material</a></li>
                 <li><a href='https://www.youtube.com/channel/UC5d03LHy5Ny82hrnhw3ZMTQ'><FaYoutube/> Youtube</a></li>
                 <li><Link to="/gallery"><FaChartBar /> Gallery</Link></li>
                 <li><Link to="/contact"><IoIosContact /> Contact</Link></li>
-
             </ul>
         </nav>
     );
